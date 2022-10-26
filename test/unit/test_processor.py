185268de214a2631211674e68156590beaa03838
William J. Johnson <85764770+williamjjohnson@users.noreply.github.com>
2022-10-26 15:21:13 -0600

    Add unit test file for data_processor.py

---
+++ test/unit/test_processor.py
@@ -0,0 +1,15 @@
+import sys
+import unittest
+import random
+sys.path.append('../../')
+import data_processor.py #nopep8
+
+class TestProcessor(unittest.TestCase):
+
+	@classmethod
+
+	def setUpClass(cls):
+
+	@classmethod
+
+	def tearDownClass(cls):
