3c6cec70f76b470ef8e7f60ed1774a092730dc25
William J. Johnson <85764770+williamjjohnson@users.noreply.github.com>
2022-10-26 20:01:59 -0600

    Add unit tests for get_random_matrix

--- test/unit/test_processor.py
+++ test/unit/test_processor.py
@@ -1,15 +1,38 @@
 import sys
 import unittest
 import random
+import numpy as np
 sys.path.append('../../')
-import data_processor.py #nopep8
+import data_processor as dtp #nopep8
 
 class TestProcessor(unittest.TestCase):
 
-	@classmethod
+#	@classmethod
+#	def setUpClass(cls):
 
-	def setUpClass(cls):
+#	@classmethod
+#	def tearDownClass(cls):
 
-	@classmethod
+	def test_get_random_matrix(self):
+		np.random.seed(17)
+		N = random.randint(1, 10)
+		# ensure N and M are different for negative test
+		M = random.randint(N + 1, N + 11)
 
-	def tearDownClass(cls):
+		random_matrix = np.random.rand(N, M)
+		wrong_matrix = np.random.rand(M, N)
+
+		# positive test : test that the correct matrix is returned
+		self.assertEqual(random_matrix.all(), dtp.get_random_matrix(N, M).all())
+
+		# negative test : test that the returned matrix doesn't
+		# equal another matrix
+		self.assertNotEqual(wrong_matrix.all(), dtp.get_random_matrix(M, N).all())
+
+		# exception test : test that an exception is raised if
+		# N and M are not integers or less than zero
+		self.assertRaises(Exception, dtp.get_random_matrix, 1.231, 7.890)
+		self.assertRaises(Exception, dtp.get_random_matrix, -12, -1)
+
+if __name__ == '__main__':
+	unittest.main()
