7315e490f3cb15408d61cf34286cbdb31ff96039
William J. Johnson <85764770+williamjjohnson@users.noreply.github.com>
2022-10-27 09:56:43 -0600

    Add unit test for write_matrix_to_file

--- data_processor.py
+++ data_processor.py
@@ -1,24 +1,27 @@
 import numpy as np
 import random
+import pandas as pd
 
 def get_random_matrix(num_rows: int, num_columns: int, rand_seed = None):
-	'''
-	Inputs:
-		two integers greater than zero and
-		an optional random seed argument
-	Returns:
-		matrix with N rows and M columns populated with 
-		floats from a uniform distribution between 0 and 1
-	'''
-	if rand_seed is not None:
-		np.random.seed(rand_seed)
+    '''
+    Inputs:
+        two integers greater than zero and
+        an optional random seed argument
+    Returns:
+        matrix with N rows and M columns populated with 
+        floats from a uniform distribution between 0 and 1
+    '''
+    if rand_seed is not None:
+        np.random.seed(rand_seed)
 
-	matrix = np.random.rand(num_rows, num_columns)
+    matrix = np.random.rand(num_rows, num_columns)
 
-	return matrix
+    return matrix
 
 def get_file_dimensions(file_name):
-	return (0,0)
+    df = pd.read_csv(file_name, header = None)
+    dimensions = df.shape
+    return dimensions
 
 def write_matrix_to_file(num_rows, num_columns, file_name):
-	return None
+    return None
--- test/unit/test_processor.py
+++ test/unit/test_processor.py
@@ -3,31 +3,45 @@ import unittest
 import random
 import numpy as np
 import pandas as pd
+import filecmp
+import os
 sys.path.append('../../')
 import data_processor as dtp  # nopep8
 
+class TestData(unittest.TestCase):
 
-class TestProcessor(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.rand_seed = random.randint(1, 100)
+        # ensure different matrices for negative test with wrong_seed
+        cls.wrong_seed = random.randint(cls.rand_seed + 1, cls.rand_seed + 50)
+        cls.N = random.randint(1, 10)
+        cls.M = random.randint(1, 10)
+        np.random.seed(cls.rand_seed)
+        cls.random_matrix = np.random.rand(cls.N, cls.M)
+        np.random.seed(cls.wrong_seed)
+        cls.wrong_matrix = np.random.rand(cls.N, cls.M)
+        pd.DataFrame(cls.random_matrix).to_csv(
+            "test_files/random.csv", header = False)
+        pd.DataFrame(cls.wrong_matrix).to_csv(
+            "test_files/wrong.csv", header = False)
+         
+    @classmethod
+    def tearDownClass(cls):
+        print("Running tearDownClass")
+        dir = "./test_files/"
+        for f in os.listdir(dir):
+            os.remove(os.path.join(dir, f))
 
     def test_get_random_matrix(self):
-        rand_seed = random.randint(1, 100)
-        # ensure different matrices for negative test with wrong_seed
-        wrong_seed = random.randint(rand_seed + 1, rand_seed + 50)
-        N = random.randint(1, 10)
-        M = random.randint(1, 10)
-        test_matrix = dtp.get_random_matrix(N, M, rand_seed)
-
-        np.random.seed(rand_seed)
-        random_matrix = np.random.rand(N, M)
-        np.random.seed(wrong_seed)
-        wrong_matrix = np.random.rand(N, M)
+        test_matrix = dtp.get_random_matrix(self.N, self.M, self.rand_seed)
 
         # positive test : test that the correct matrix is returned
-        self.assertTrue(np.all(np.equal(test_matrix, random_matrix)))
+        self.assertTrue(np.all(np.equal(test_matrix, self.random_matrix)))
 
         # negative test : test that the returned matrix doesn't
         # equal another matrix
-        self.assertTrue(np.any(np.not_equal(test_matrix, wrong_matrix)))
+        self.assertTrue(np.any(np.not_equal(test_matrix, self.wrong_matrix)))
 
         # exception test : test that an exception is raised if
         # N and M are not integers or less than zero
@@ -44,6 +58,20 @@ class TestProcessor(unittest.TestCase):
         # negative test : test that function output
         # doesn't equal the wrong dimensions
         self.assertNotEqual(dimensions, 10, 15)
+
+    def test_write_matrix_to_file(self):
+        write_matrix_to_file(
+            self.N, self.M, self.rand_seed, "test_files/test.csv")
+
+        # positive test : test that the correct file is written
+        self.assertTrue(
+            filecmp.cmp("test_files/test.csv", "test_files/random.csv"))
+
+        # negative test : test that an incorrect file doesn't equal
+        # the written file
+        self.assertNotTrue(
+            filecmp.cmp("test_files/test.csv", "test_files/wrong.csv"))
+
 
 if __name__ == '__main__':
     unittest.main()
