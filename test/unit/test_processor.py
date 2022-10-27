4838298b3f4358309f728178d39d0f69d29ee8ac
William J. Johnson <85764770+williamjjohnson@users.noreply.github.com>
2022-10-27 02:24:59 -0600

    Add unit test for get_file_dimensions

--- test/unit/test_processor.py
+++ test/unit/test_processor.py
@@ -2,41 +2,48 @@ import sys
 import unittest
 import random
 import numpy as np
+import pandas as pd
 sys.path.append('../../')
-import data_processor as dtp #nopep8
+import data_processor as dtp  # nopep8
+
 
 class TestProcessor(unittest.TestCase):
 
-#	@classmethod
-#	def setUpClass(cls):
+    def test_get_random_matrix(self):
+        rand_seed = random.randint(1, 100)
+        # ensure different matrices for negative test with wrong_seed
+        wrong_seed = random.randint(rand_seed + 1, rand_seed + 50)
+        N = random.randint(1, 10)
+        M = random.randint(1, 10)
+        test_matrix = dtp.get_random_matrix(N, M, rand_seed)
 
-#	@classmethod
-#	def tearDownClass(cls):
+        np.random.seed(rand_seed)
+        random_matrix = np.random.rand(N, M)
+        np.random.seed(wrong_seed)
+        wrong_matrix = np.random.rand(N, M)
 
-	def test_get_random_matrix(self):
-		rand_seed = random.randint(1, 100)
-		# ensure different matrices for negative test with wrong_seed
-		wrong_seed = random.randint(rand_seed + 1, rand_seed + 50)
-		N = random.randint(1, 10)
-		M = random.randint(1, 10)
-		test_matrix = dtp.get_random_matrix(N, M, rand_seed)
+        # positive test : test that the correct matrix is returned
+        self.assertTrue(np.all(np.equal(test_matrix, random_matrix)))
 
-		np.random.seed(rand_seed)
-		random_matrix = np.random.rand(N, M)
-		np.random.seed(wrong_seed)
-		wrong_matrix = np.random.rand(N, M)
+        # negative test : test that the returned matrix doesn't
+        # equal another matrix
+        self.assertTrue(np.any(np.not_equal(test_matrix, wrong_matrix)))
 
-		# positive test : test that the correct matrix is returned
-		self.assertTrue(np.all(np.equal(test_matrix, random_matrix)))
+        # exception test : test that an exception is raised if
+        # N and M are not integers or less than zero
+        self.assertRaises(Exception, dtp.get_random_matrix, 1.231, 7.890)
+        self.assertRaises(Exception, dtp.get_random_matrix, -12, -1)
 
-		# negative test : test that the returned matrix doesn't
-		# equal another matrix
-		self.assertTrue(np.any(np.not_equal(test_matrix, wrong_matrix)))
+    def test_get_file_dimesions(self):
+        csv_name = "iris.data"
+        dimensions = get_file_dimensions(csv_name)
 
-		# exception test : test that an exception is raised if
-		# N and M are not integers or less than zero
-		self.assertRaises(Exception, dtp.get_random_matrix, 1.231, 7.890)
-		self.assertRaises(Exception, dtp.get_random_matrix, -12, -1)
+        # positive test : test that the correct dimensions are returned
+        self.assertEqual(dimensions, (150, 5))
+
+        # negative test : test that function output
+        # doesn't equal the wrong dimensions
+        self.assertNotEqual(dimensions, 10, 15)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
