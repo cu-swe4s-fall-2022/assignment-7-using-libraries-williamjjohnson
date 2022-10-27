c7e1aaaec2e23f4c9abad38c46e2a937e7c7f8c0
William J. Johnson <85764770+williamjjohnson@users.noreply.github.com>
2022-10-27 12:47:50 -0600

    Add plots

--- data_processor.py
+++ data_processor.py
@@ -2,26 +2,51 @@ import numpy as np
 import random
 import pandas as pd
 
-def get_random_matrix(num_rows: int, num_columns: int, rand_seed = None):
+
+def get_random_matrix(num_rows: int, num_columns: int, rand_seed=None):
     '''
     Inputs:
         two integers greater than zero and
         an optional random seed argument
     Returns:
-        matrix with N rows and M columns populated with 
+        matrix with N rows and M columns populated with
         floats from a uniform distribution between 0 and 1
     '''
     if rand_seed is not None:
         np.random.seed(rand_seed)
-
     matrix = np.random.rand(num_rows, num_columns)
-
     return matrix
 
+
 def get_file_dimensions(file_name):
-    df = pd.read_csv(file_name, header = None)
+    '''
+    Inputs:
+        the name of a csv file
+    Outputs:
+        the dimensions of the contents of the
+        file as a tuple (rows, columns)
+    '''
+    df = pd.read_csv(file_name, header=None)
     dimensions = df.shape
     return dimensions
 
-def write_matrix_to_file(num_rows, num_columns, file_name):
+
+def write_matrix_to_file(
+        num_rows: int, num_columns: int, file_name: str, rand_seed=None):
+    '''
+    Inputs:
+        two integers greater than zero,
+        an optional random seed argument,
+        and a file name to write to
+    Outputs:
+        Writes a file of a DataFrame with
+        N rows and M columns populated with
+        floats from a uniform distribution
+        between 0 and 1
+    Returns:
+        None
+    '''
+    df = get_random_matrix(
+        num_rows, num_columns, rand_seed)
+    pd.DataFrame(df).to_csv(file_name, header = None)
     return None
--- plotter.py
+++ plotter.py
@@ -1,1 +1,47 @@
-# Your code to create majestic plots goes in here!
+import matplotlib.pyplot as plt
+import pandas as pd
+
+iris = pd.read_csv('iris.data', header=None)
+iris.columns = [
+    'sepal_width','sepal_length','petal_width',
+    'petal_length','species']
+
+# Plot boxplot for each measurement of iris
+measurement_names = [
+    'sepal_length','sepal_width','petal_width','petal_length']
+plt.figure()
+plt.boxplot(iris[measurement_names],labels=measurement_names)
+plt.ylabel('cm')
+plt.savefig("iris_boxplot.png")
+
+# plot scatter plot of petal length vs width with iris species
+# colored differently
+plt.figure()
+for species_name in set(iris['species']):
+    iris_subset = iris[iris['species'] == species_name]
+    plt.scatter(iris_subset['petal_length'], iris_subset['petal_width'], 
+         label=species_name, s=5)
+plt.legend()
+plt.xlabel('petal_length (cm)')
+plt.ylabel('petal_width (cm)')
+plt.savefig("iris_scatter.png")
+
+# plot the above two plots as subplots
+plt.figure()
+fig, axes = plt.subplots(1,2)
+fig.set_size_inches(10,5)
+
+# left plot
+axes[0].boxplot(iris[measurement_names],labels=measurement_names)
+axes[0].set_ylabel('cm')
+
+# right plot
+for species_name in set(iris['species']):
+    iris_subset = iris[iris['species'] == species_name]
+    axes[1].scatter(iris_subset['petal_length'], iris_subset['petal_width'], 
+         label=species_name, s=5)
+axes[1].legend()
+axes[1].set_xlabel('petal_length (cm)')
+axes[1].set_ylabel('petal_width (cm)')
+
+plt.savefig('combo_plot.png')
